2d7b4ea3ef547656d2ebce663f9a252880dfe550
Julien Viet <julien@julienviet.com>
2015-11-06 08:38:14 +0100

    Implement ProcessReadStream pause/resume

--- src/main/java/io/vertx/ext/childprocess/impl/ProcessReadStreamImpl.java
+++ src/main/java/io/vertx/ext/childprocess/impl/ProcessReadStreamImpl.java
@@ -15,26 +15,39 @@ public class ProcessReadStreamImpl implements ProcessReadStream {
   private final Context context;
   private Handler<Buffer> handler;
   private Handler<Void> endHandler;
+  private ByteBuffer processBuffer;
+  private boolean paused;
 
   public ProcessReadStreamImpl(Context context) {
     this.context = context;
   }
 
-  void write(ByteBuffer byteBuffer) {
-    byte[] bytes = new byte[byteBuffer.remaining()];
-    byteBuffer.get(bytes);
-    Buffer buffer = Buffer.buffer(bytes);
-    context.runOnContext(v -> {
-      if (handler != null) {
-        handler.handle(buffer);
-      }
-    });
+  synchronized void write(ByteBuffer byteBuffer) {
+    if (!paused) {
+      sendBuffer(byteBuffer);
+    } else {
+      // It's always the same buffer
+      processBuffer = byteBuffer;
+    }
   }
 
   void close() {
     context.runOnContext(v -> {
       if (endHandler != null) {
         endHandler.handle(null);
+      }
+    });
+  }
+
+  private void sendBuffer(ByteBuffer byteBuffer) {
+    byte[] bytes = new byte[byteBuffer.remaining()];
+    byteBuffer.get(bytes);
+    Buffer buffer = Buffer.buffer(bytes);
+    int len = buffer.length();
+    System.out.println("L=" + len);
+    context.runOnContext(v -> {
+      if (handler != null) {
+        handler.handle(buffer);
       }
     });
   }
@@ -51,12 +64,20 @@ public class ProcessReadStreamImpl implements ProcessReadStream {
   }
 
   @Override
-  public ProcessReadStream pause() {
+  public synchronized ProcessReadStream pause() {
+    paused = true;
     return this;
   }
 
   @Override
-  public ProcessReadStream resume() {
+  public synchronized ProcessReadStream resume() {
+    if (paused) {
+      paused = false;
+      if (processBuffer != null) {
+        sendBuffer(processBuffer);
+        processBuffer = null;
+      }
+    }
     return this;
   }
 
