0f790e0104908ac7bcb419592a2f270b7db0ba60
Mike_Burns <5823252+Mikey-Burns@users.noreply.github.com>
2018-12-12 19:06:38 +0000

    Add generated class
    Signed-off-by: Mike_Burns <5823252+Mikey-Burns@users.noreply.github.com>

--- src/main/generated/com/julienviet/childprocess/ProcessOptionsConverter.java
+++ src/main/generated/com/julienviet/childprocess/ProcessOptionsConverter.java
@@ -18,35 +18,48 @@ package com.julienviet.childprocess;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter for {@link com.julienviet.childprocess.ProcessOptions}.
- *
  * NOTE: This class has been automatically generated from the {@link com.julienviet.childprocess.ProcessOptions} original class using Vert.x codegen.
  */
 public class ProcessOptionsConverter {
 
-  public static void fromJson(JsonObject json, ProcessOptions obj) {
-    if (json.getValue("cwd") instanceof String) {
-      obj.setCwd((String)json.getValue("cwd"));
-    }
-    if (json.getValue("env") instanceof JsonObject) {
-      java.util.Map<String, java.lang.String> map = new java.util.LinkedHashMap<>();
-      json.getJsonObject("env").forEach(entry -> {
-        if (entry.getValue() instanceof String)
-          map.put(entry.getKey(), (String)entry.getValue());
-      });
-      obj.setEnv(map);
+  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ProcessOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "cwd":
+          if (member.getValue() instanceof String) {
+            obj.setCwd((String)member.getValue());
+          }
+          break;
+        case "env":
+          if (member.getValue() instanceof JsonObject) {
+            java.util.Map<String, java.lang.String> map = new java.util.LinkedHashMap<>();
+            ((Iterable<java.util.Map.Entry<String, Object>>)member.getValue()).forEach(entry -> {
+              if (entry.getValue() instanceof String)
+                map.put(entry.getKey(), (String)entry.getValue());
+            });
+            obj.setEnv(map);
+          }
+          break;
+      }
     }
   }
 
   public static void toJson(ProcessOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+  public static void toJson(ProcessOptions obj, java.util.Map<String, Object> json) {
     if (obj.getCwd() != null) {
       json.put("cwd", obj.getCwd());
     }
     if (obj.getEnv() != null) {
       JsonObject map = new JsonObject();
-      obj.getEnv().forEach((key,value) -> map.put(key, value));
+      obj.getEnv().forEach((key, value) -> map.put(key, value));
       json.put("env", map);
     }
   }
